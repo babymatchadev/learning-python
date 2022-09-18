0613ac984eb8f4d7244fa2668ad2d334e5297a3c
babymatchadev <ziebawiktoria0@gmail.com>
2022-09-18 19:45:16 +0200

    First commit

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,18 @@
+from random import choice
+
 print('Hello baby! It is your first line of code')
 
 favorite_food = ['Pizza', 'Ramen', 'Bubble tea']
 
-for food in favorite_food:
-    print(f'I absolutely love {food} I could it eat all the time \n')
+# for food in favorite_food:
+#     print(f'I absolutely love {food} I could it eat all the time \n')
 
-print('Test')
+# print('Test')
+
+
+# list of animes
+animes = ['bleach', 'naruto', 'fairy tail', 'gintama', 'boku no hero', 'black clover']
+# print a random anime from animes
+randomly_choosen_anime = choice(animes)
+
+print(f'You should watch {randomly_choosen_anime.upper()}')
