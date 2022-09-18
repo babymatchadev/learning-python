5cf77277275e47601728b272cee1b92f6e3ec4ca
babymatchadev <ziebawiktoria0@gmail.com>
2022-09-18 18:04:54 +0200

    First ever init

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+print('Hello baby! It is your first line of code')
+
+favorite_food = ['Pizza', 'Ramen', 'Bubble tea']
+
+for food in favorite_food:
+    print(f'I absolutely love {food} I could it eat all the time \n')
+
+print('Test')
